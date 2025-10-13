6421d38cade74432eef0437575113d100d8679a5
KristiyanHr <20118@uktc-bg.com>
2025-10-13 09:25:56 +0300

    Some more files from 13/10/25

--- min_to_hour.py
+++ min_to_hour.py
@@ -1,7 +1,14 @@
 #да преобразуваме минути в часове
 
-minutes = float(input("Please enter minutes: "))
-hours = minutes // 60
-remainingMinutes = minutes % 60
+total_minutes = float(input("Please enter minutes: "))
+days = total_minutes // 1440
+remainingMinutes = total_minutes % 1440
+hours = remainingMinutes // 60
+minutes = remainingMinutes % 60
 
 print(f"{hours} hours and {remainingMinutes} minutes")
+
+#преобразуване на минути в дни часове и минути
+
+print(f"{minutes} minutes is equal to {days} days, {hours} hours, and {minutes} minutes.")
+
---
+++ squareRoot.py
@@ -0,0 +1,6 @@
+#квадратен корен на прочртеното число n
+import math
+
+n = int(input("Enter a number: "))
+
+print(math.sqrt(n))
